76b5a91d539426178ee7fa0190f095c2e4d982cb
IbraheimTarek <77370509+IbraheimTarek@users.noreply.github.com>
2022-11-08 09:01:26 +0200

    atlas

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ const app = express();
 const path = require("path");
 const method = require("method-override");
 
-app.engine("ejs", ejsMate);
+app.engine("ejs", ejsMate); //to include the headers and the partial templates
 
+//ejs setup and getting ejs files from the views directory
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// getting styles and js fronten files from the public dirctory
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(method("_method"));
 
+// handling users http requests
 app.use("/", async (req, res) => {
   res.render("pages/home");
 });
+
+//to open the server
 app.listen(3000, () => {
   console.log("Serving on port 3000");
 });
