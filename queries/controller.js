45b8ca5b3676d723d3a715ac39fe8f19e76e65c1
A-OMAR-1234 <113605371+A-OMAR-1234@users.noreply.github.com>
2022-12-26 03:45:06 +0200

    hi hema

--- queries/controller.js
+++ queries/controller.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "bogo", //passwordchanges
+  password: "qqqq1111", //passwordchanges
   database: "mydb",
 });
 
